f75f47d73dcb233b547c18dc7b3eb3914424c5e5
parth020806 <parthchugh750@gmail.com>
2022-08-25 12:10:30 +0530

    admin.py done

--- rapidrecruits/apis/admin.py
+++ rapidrecruits/apis/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+admin.site.register(models.ApplicantInfoModel)
+admin.site.register(models.CollegeInfoModel)
+admin.site.register(models.EmployeeInfoModel)
+admin.site.register(models.VacanciesInfoModel)
+admin.site.register(models.RecruitmentCommitteeInfoModel)
+admin.site.register(models.ApplicantQualificationModel)
+admin.site.register(models.ApplicantExperienceModel)
+admin.site.register(models.VacancyApplicantMapping)
